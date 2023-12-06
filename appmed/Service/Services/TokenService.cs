cd29970dc04ad344d7f93b8d44afc4daf8a63f13
felipesantos22 <felipevs2018@gmail.com>
2023-12-06 16:51:54 -0300

    feat: insert token

--- appmed/Service/Services/TokenService.cs
+++ appmed/Service/Services/TokenService.cs
@@ -1,6 +1,31 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using appmed.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+
 namespace appmed.Service.Services;
 
 public class TokenService
 {
-    
+    public static object GenerateToken(Employee employee)
+    {
+        var Key = Encoding.ASCII.GetBytes(Services.Key.Secret);
+        var tokenConfig = new SecurityTokenDescriptor
+        {
+            Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+            {
+                new Claim("employeeId", employee.Id.ToString()),
+            }),
+            Expires = DateTime.UtcNow.AddHours(3),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Key),SecurityAlgorithms.HmacSha256Signature)
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenConfig);
+        var tokenString = tokenHandler.WriteToken(token);
+        return new
+        {
+            token = tokenString,
+        };
+    }
 }
